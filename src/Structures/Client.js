50b397b1d7b57719a01739b23d85f2635b6f79ec
Derpinou <lightcompte@gmail.com>
2021-12-17 10:13:25 +0100

    Semicolon ;

--- src/Structures/Client.js
+++ src/Structures/Client.js
@@ -1,5 +1,5 @@
 const { Client } = require('discord.js') ,
-    {TicketManager} = require('discord-tickets') ;
+    { TicketManager } = require('discord-tickets') ;
 
 module.exports = class TicketBot extends Client {
     constructor(options) {
@@ -14,25 +14,25 @@ module.exports = class TicketBot extends Client {
 
     eventLoader() {
         readdir("./src/events", (err, files) => {
-            if (!files) return;
-            if (err) this.emit("error", err);
+            if (!files) return ;
+            if (err) this.emit("error", err) ;
             for (const dir of files) {
                 readdir(`./src/events/${dir}`, (err, file) => {
-                    if (!file) return;
-                    if (err) this.emit("error", err);
+                    if (!file) return ;
+                    if (err) this.emit("error", err) ;
                     for (const evt of file) {
                         try {
-                            if (!evt) return;
-                            const event = new (require(`../events/${dir}/${evt}`))(this);
-                            this.logger.log(`${evt} chargé`);
-                            super.on(evt.split(".")[0], (...args) => event.run(...args));
+                            if (!evt) return ;
+                            const event = new (require(`../events/${dir}/${evt}`))(this) ;
+                            this.logger.log(`${evt} chargé`) ;
+                            super.on(evt.split(".")[0], (...args) => event.run(...args)) ;
                         } catch (e) {
-                            this.emit("error", `${evt} n'a pas chargé ${e.stack}`)
+                            this.emit("error", `${evt} n'a pas chargé ${e.stack}`) ;
                         }
                     }
                 })
             }
-        });
-        return this
+        }) ;
+        return this ;
     }
 }
